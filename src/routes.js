f8a1993ab46de785622f5fc5f60f1be9dbec9d68
Njeri Ngigi <njery.ngigi@gmail.com>
2019-10-07 14:00:00 +0200

    update express router

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { sayHello } = require('./controllers/hello');
 
-const router = express();
+const router = express.Router();
 
 router.get('/', sayHello);
 
